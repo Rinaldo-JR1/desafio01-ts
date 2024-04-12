41b89d77c9534da61e6b530486feecdbe6ecd845
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-04-12 13:56:10 -0300

    Fim

--- app.ts
+++ app.ts
@@ -3,23 +3,23 @@ import { ExtraAccount } from "./class/ExtraAccount";
 import { PeopleAccount } from "./class/PeopleAccount";
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, "Nath", 10);
-console.info(`People account \n`)
+console.info(`People account \n`);
 peopleAccount.withdraw(5);
 peopleAccount.deposit(100);
 peopleAccount.withdraw(5);
 peopleAccount.getBalance();
-console.log(`\n`)
+console.log(`\n`);
 
 const companyAccount: CompanyAccount = new CompanyAccount("DIO", 20);
-console.info(`Company account \n`)
+console.log(`Company account \n`);
 companyAccount.withdraw(5);
 companyAccount.deposit(100);
 companyAccount.withdraw(5);
 companyAccount.getBalance();
-console.log(`\n`)
+console.log(`\n`);
 
 const extraAccount: ExtraAccount = new ExtraAccount("Nath", 30);
-console.info(`Extra account \n`)
+console.log(`Extra account \n`);
 extraAccount.withdraw(5);
 extraAccount.deposit(100);
 extraAccount.withdraw(5);
